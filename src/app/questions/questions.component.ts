f8bd1f02ef1203a66ba111f60604cf4b23e9dafd
Daniel Muñoz <51991417+DaniPK7@users.noreply.github.com>
2021-10-20 13:59:54 +0200

    Mas cosas

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument} from '@angular/fire/compat/firestore';
+import { SelectControlValueAccessor } from '@angular/forms';
 import { Observable } from 'rxjs';
 
 //let nQstOptions= new Map<string, number>();
@@ -19,12 +20,14 @@ export class QuestionsComponent implements OnInit {
   prueba: Observable<any[]>;
   sPrueba: Observable<any[]> | undefined;
   sPrueba2: Observable<any[]> | undefined;
+  sPrueba3: Observable<any[]> | undefined;
   
   
   nQstOptions: Map<string, number> = new Map<string, number>();
   
   
-  
+  aaa: Map<string, Observable<any[]>> = new Map<string, Observable<any[]>>();
+  BBB: Array<Map<any,any>>=new Array;
 
   constructor(firestore: AngularFirestore) {
     this.items = firestore.collection('questions').valueChanges();
@@ -39,31 +42,61 @@ export class QuestionsComponent implements OnInit {
       {
         subPrueba.docs.forEach(async doc=>
           {
-            //console.log(doc.id,"---",doc);
-            let i=0;
+            if(doc.id=="Q1"){
+              this.sPrueba=firestore.collection('prueba').doc(doc.id).collection('subPrueba').valueChanges();
+            }
+            else if(doc.id=="Q2"){
+              this.sPrueba2=firestore.collection('prueba').doc(doc.id).collection('subPrueba').valueChanges();
+            } 
+            else if(doc.id=="Q3"){
+              this.sPrueba3=firestore.collection('prueba').doc(doc.id).collection('subPrueba').valueChanges();
+            }
+            /* let i=0;
             this.nQstOptions.set(doc.id,i);
-            
+        
             const subQuerySnapshot=firestore.collection('prueba').doc(doc.id).collection('subPrueba').get();
             subQuerySnapshot.forEach(o=>
               {
-                
                 o.docs.forEach(sO=>
                   {
+                    this.aaa.set(doc.id,firestore.collection('prueba').doc(doc.id).collection('subPrueba').valueChanges())
+                    this.BBB.push(this.aaa);
                     i++;
                     this.nQstOptions.set(doc.id, i);
                   });
-                  
+                  console.log(this.BBB);
                   console.log(doc.id,"---",this.nQstOptions.get(doc.id));
-              });
-             });
+              }); */
+            });
       }
     );
     
-    this.sPrueba=firestore.collection('prueba').doc('Q1').collection('subPrueba').valueChanges();
+    //this.sPrueba=firestore.collection('prueba').doc('Q1').collection('subPrueba').valueChanges();
     
   }
 
   ngOnInit(): void {
   }
+  ShowMsg()
+  {
+    let selected=new Array;
+    var x= document.getElementsByClassName("chckBx");
+    for(let i=0; i<x.length;i++)
+    {
+      var a= x[i] as HTMLInputElement;
+      if(a.checked){selected.push(a);}
+    } 
+    //alert(selected.length);
+    let b:string="";
+    if(selected.length!=0)
+    {
+      for(let i=0; i<selected.length;i++)
+      {
+        b+selected[i].value+" ";
+      }
+      alert(b);
+    }
+    
 
+  }
 }
